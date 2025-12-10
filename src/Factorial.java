29eb1f388b8d7af57905149c6c2087e55dd1b76d
unknown <avelmar@g.educaand.es>
2025-12-10 08:35:30 +0100

    B: Versión con función pedirNumero

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,13 +2,16 @@
 import java.util.Scanner;
 
 public class Factorial {
+	private static Scanner sc = new Scanner(System.in);
 
+	private static int pedirNumero() {
+	    System.out.print("Introduce un número entero mayor o igual que 0: ");
+	    return sc.nextInt();
+	}
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Introduce un número entero mayor o igual que 0: ");
-        int num = sc.nextInt();
-
+        sc = new Scanner(System.in);
+     int num= pedirNumero();
+     
         if (num < 0) {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
         } else {
