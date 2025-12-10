6d6111e781b318341d2da7184727ae7b73dbc967
unknown <avelmar@g.educaand.es>
2025-12-10 09:12:14 +0100

    E:Función iterativa

--- src/Factorial.java
+++ src/Factorial.java
@@ -23,6 +23,17 @@ public class Factorial {
 
 	    return num;
 	}
+	
+	private static long factorialBucle(int n) {
+
+	    long fac = 1;
+	    for (int i = 1; i <= n; i++) {
+	        fac *= i;
+	    }
+	    return fac;
+	}
+ 
+	
 	public static void main(String[] args) {
         sc = new Scanner(System.in);
      int num= pedirNumero();
